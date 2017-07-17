f9cecd91a382cb9a139a229ad63f4ea4de24f49b
hanpenghu <563909408@qq.com>
2017-07-17 15:19:21 +0800

    2017_7_17   weekday(1)   15:19:18

--- src/main/java/com/hanhan/test1/controller/Test2.java
+++ src/main/java/com/hanhan/test1/controller/Test2.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
- * Created by Administrator on 2017-07-17.
+ * Created by 韩寒 on 2017-07-17.
  * @RestController无法return一个路径,但是: @Controller可以返回一个路径
  */
 @Controller
